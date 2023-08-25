39413718cf9262a8190e8c76c69db1895dc99d8a
VladimirKrasov <vladimirkrasov.ex@gmail.com>
2023-08-25 16:37:52 +0300

    Remove build folder Add deploy package

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {ThemeProvider, createTheme} from '@mui/material/styles'
+import {BrowserRouter} from 'react-router-dom'
+
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
@@ -12,9 +14,11 @@ const theme = createTheme({
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>,
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>,
 )
 
 // If you want to start measuring performance in your app, pass a function
